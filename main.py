9728a186bca31e48b2c65d308ac36bfad3dc5ec3
adityavkulkarni <adityavkulkarni0999@gmail.com>
2024-03-24 14:13:53 -0500

    misc

--- main.py
+++ main.py
@@ -27,29 +27,28 @@ def parse_kb_file(filepath):
 
 def theorem_prover(kb, clause_to_test):
     global PROCESSED_KB
-    negated_clause = negate_clause(clause_to_test)
-    ind = len(kb) + 1
     PROCESSED_KB = set(map(frozenset, kb))
+    ind = len(kb) + 1
     for i, cl in enumerate(kb, start=1):
         print_stdout(f"{i}. {' '.join(cl)} {{}}")
-    for c in negated_clause:
+    for c in negate_clause(clause_to_test):
         kb.append([c])
         print_stdout(f"{ind}. {c} {{}}")
-        PROCESSED_KB.add(frozenset((c, )))
+        PROCESSED_KB.add(frozenset((c,)))
         ind += 1
     for cli, clause1 in enumerate(kb, start=1):
         for clj, clause2 in enumerate(kb[:cli], start=1):
-            result = resolve(clause1, clause2)
-            if result is False:
+            status, result = resolve(clause1, clause2)
+            if status is False:
                 print_stdout(f"{ind}. Contradiction {{{cli}, {clj}}}")
                 print_stdout("Valid")
                 return
-            elif result is not True and frozenset(result) not in PROCESSED_KB:
+            elif status and frozenset(result) not in PROCESSED_KB:
                 PROCESSED_KB.add(frozenset(result))
                 kb.append(result)
                 print_stdout(f"{ind}. {' '.join(result)} {{{cli}, {clj}}}")
                 ind += 1
-    print_stdout('Not Valid')
+    print_stdout('Fail')
 
 
 def negate_clause(clause):
@@ -71,13 +70,16 @@ def resolve(c1, c2):
                 resolved.remove(l1)
                 resolved.remove(l2)
                 if len(resolved) == 0:
-                    return False
+                    return False, []
                 elif any(r1 == ('~' + r2) or r2 == ('~' + r1) for r2 in resolved for r1 in resolved):
-                    return True
+                    return None, []
                 else:
-                    return True if set(resolved) in PROCESSED_KB else resolved
+                    if set(resolved) in PROCESSED_KB:
+                        return None, []
+                    else:
+                        return True, resolved
     if resolved == ors:
-        return True
+        return None, []
 
 
 def print_stdout(txt):
@@ -87,24 +89,24 @@ def print_stdout(txt):
 
 
 def test(path):
-    print("Execution time: {:.3f} seconds".format(time.time() - start_time))
+    print("[DEBUG]Execution time: {:.3f} seconds".format(time.time() - start_time))
     with open(path.replace("in", "out"), "r") as f:
         lines = [x.replace("\n", "") for x in f.readlines()]
     if len(lines) != len(OUTPUT):
-        print("Lists are not equal in length.")
+        print("[DEBUG]Lists are not equal in length.")
         l_str = '\n'.join(lines)
-        print(f"Expected:\n{l_str}\n")
+        print(f"[DEBUG]Expected:\n{l_str}\n")
         l_str = '\n'.join(OUTPUT)
-        print(f"Actual:\n{l_str}\n")
+        print(f"[DEBUG]Actual:\n{l_str}\n")
     else:
         unequal_elements = [(elem1, elem2) for elem1, elem2 in zip(lines, OUTPUT) if elem1 != elem2]
         if not unequal_elements:
-            print("Lists are equal.")
+            print("[DEBUG]Lists are equal.")
         else:
-            print("Lists are not equal.")
-            print("Elements that are not equal:")
+            print("[DEBUG]Lists are not equal.")
+            print("[DEBUG]Elements that are not equal:")
             for elem1, elem2 in unequal_elements:
-                print(f"{elem1} (Expected) != {elem2} (Actual)")
+                print(f"[DEBUG]{elem1} (Expected) != {elem2} (Actual)")
 
 
 if __name__ == '__main__':
