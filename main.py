c8402b01ae283bbe31f851f17f7c659ef69052fd
adityavkulkarni <adityavkulkarni0999@gmail.com>
2024-03-24 09:24:18 -0500

    misc

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-"""
-https://github.com/nomaanakhan/Theorem-Prover-for-Clause-Logic
-https://github.com/codeCollision4/resolution_solver
-"""
 import argparse
 import time
-from multiprocessing import Pool
 
 OUTPUT = []
+PROCESSED_KB = {}
 
 
 def parse_kb_file(filepath):
@@ -26,11 +22,37 @@ def parse_kb_file(filepath):
                     current_literal += char
             if current_literal:
                 clause.append(current_literal.strip())
-
             initial_kb.append(clause)
     last_clause = initial_kb[-1]
     initial_kb = initial_kb[:-1]
     return initial_kb, last_clause
+
+
+def theorem_prover(kb, clause_to_test):
+    global PROCESSED_KB
+    negated_clause = negate_clause(clause_to_test)
+    ind = len(kb) + 1
+    PROCESSED_KB = set(map(frozenset, kb))
+    for i, cl in enumerate(kb, start=1):
+        print_stdout(f"{i}. {' '.join(cl)} {{}}")
+    for c in negated_clause:
+        kb.append([c])
+        print_stdout(f"{ind}. {c} {{}}")
+        PROCESSED_KB.add(frozenset((c, )))
+        ind += 1
+    for cli, clause1 in enumerate(kb, start=1):
+        for clj, clause2 in enumerate(kb[:cli], start=1):
+            result = resolve(clause1, clause2)
+            if result is False:
+                print_stdout(f"{ind}. Contradiction {{{cli}, {clj}}}")
+                print_stdout("Valid")
+                return
+            elif result is not True and frozenset(result) not in PROCESSED_KB:
+                PROCESSED_KB.add(frozenset(result))
+                kb.append(result)
+                print_stdout(f"{ind}. {' '.join(result)} {{{cli}, {clj}}}")
+                ind += 1
+    print_stdout('Not Valid')
 
 
 def negate_clause(clause):
@@ -40,85 +62,25 @@ def negate_clause(clause):
     return n
 
 
-def theorem_prover(kb, clause_to_test):
-    negated_clause = negate_clause(clause_to_test)
-    ind = len(kb) + 1
-    existing_clauses = set(map(tuple, kb))
-
-    for i, cl in enumerate(kb, start=1):
-        print_stdout(f"{i}. {' '.join(cl)} {{}}")
-
-    for c in negated_clause:
-        kb.append([c])
-        print_stdout(f"{ind}. {c} {{}}")
-        existing_clauses.add((c,))
-        ind += 1
-
-    for cli, clause1 in enumerate(kb, start=1):
-        for clj, clause2 in enumerate(kb[:cli], start=1):
-            result = resolve(clause1, clause2, existing_clauses)
-            if result is False:
-                print_stdout(f"{ind}. Contradiction {{{cli}, {clj}}}")
-                print_stdout("Valid")
-                return
-            elif result is not True and tuple(result) not in existing_clauses:
-                existing_clauses.add(tuple(result))
-                kb.append(result)
-                print_stdout(f"{ind}. {' '.join(result)} {{{cli}, {clj}}}")
-                ind += 1
-    print_stdout('Not Valid')
-
-
-def resolve(c1, c2, clauses):
-    resolved2 = c1 + c2
-    hashmap = {}
-    for r1 in resolved2:
-        if r1 not in hashmap.keys():
-            hashmap[r1] = 0
-
-    resolved = list(hashmap.keys())
-    ors = list(hashmap.keys())
+def resolve(c1, c2):
+    resolved = []
+    for r1 in c1 + c2:
+        if r1 not in resolved:
+            resolved.append(r1)
+    ors = resolved
     for l1 in c1:
         for l2 in c2:
-            if is_contradiction(l1, l2):
+            if l1 == ('~' + l2) or l2 == ('~' + l1):
                 resolved.remove(l1)
                 resolved.remove(l2)
                 if len(resolved) == 0:
                     return False
-                elif impTrue(resolved):
+                elif any(r1 == ('~' + r2) or r2 == ('~' + r1) for r2 in resolved for r1 in resolved):
                     return True
                 else:
-                    if any(map(lambda cl: not Diff(resolved, cl), clauses)):
-                        return True
-                    return resolved
+                    return True if set(resolved) in PROCESSED_KB else resolved
     if resolved == ors:
         return True
-
-
-def is_contradiction(l1, l2):
-    if l1 == ('~' + l2) or l2 == ('~' + l1):
-        return True
-    else:
-        return False
-
-
-def impTrue(resolved):
-    seen = set()
-    for i, r1 in enumerate(resolved):
-        for r2 in resolved[i + 1:]:
-            if (r1, r2) in seen or (r2, r1) in seen:
-                continue
-            if is_contradiction(r1, r2):
-                return True
-            seen.add((r1, r2))
-    return False
-
-
-
-def Diff(li1, li2):
-    set1 = set(li1)
-    set2 = set(li2)
-    return list(set1.symmetric_difference(set2))
 
 
 def print_stdout(txt):
